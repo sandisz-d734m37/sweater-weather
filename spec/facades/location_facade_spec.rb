5f754a6e079e6a0a342346330e6d1b0adc44a221
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-11 18:51:18 -0600

    Test: LocationFacade creates a location PORO

---
+++ spec/facades/location_facade_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe LocationFacade do
+  before do
+    @location = LocationFacade.get_location("Denver,CO")
+    VCR.insert_cassette("gets_a_location_with_a_string")
+  end
+
+  after do
+    VCR.eject_cassette
+  end
+
+  it "creates a PORO with proper attributes" do
+    expect(@location).to be_a(Location)
+    expect(@location.query).to eq("Denver,CO")
+    expect(@location.country).to eq("US")
+    expect(@location.state).to eq("CO")
+    expect(@location.county).to eq("Denver County")
+    expect(@location.city).to eq("Denver")
+    expect(@location.lat).to eq(39.738453)
+    expect(@location.lon).to eq(-104.984853)
+  end
+end
