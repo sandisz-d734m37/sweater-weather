5fdc8f43de8760d8348050db8848be594d5bf277
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-11 21:13:25 -0600

    Test: update WeatherService spec to work with VCR

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,15 +1,12 @@
 require "rails_helper"
 
 describe WeatherService do
-  it "gets weather data using latitude & longitude", :vcr do
-    weather_data = WeatherService.get_weather(39.738453, -104.984853)
+  it "gets weather data using latitude & longitude" do
+    weather_data = WeatherService.get_weather_data(39.738453, -104.984853)
 
     expect(weather_data).to be_a(Hash)
     expect(weather_data).to have_key(:current)
     expect(weather_data).to have_key(:hourly)
     expect(weather_data).to have_key(:daily)
-    expect(weather_data[:current][:temp]).to eq("Denver,CO")
-    expect(weather_data[:hourly][0][:temp][:max]).to eq("Denver")
-    expect(weather_data[:daily][0][:temp]).to eq("Denver")
   end
 end
