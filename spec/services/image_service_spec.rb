008b18851f4bdcc9df4266faab996608b02fd132
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-12 14:39:22 -0600

    Create Image Service

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -1,16 +1,19 @@
 require "rails_helper"
 
 describe ImageService do
-  it "gets image data" do
+  it "gets image data", :vcr do
     image_data = ImageService.get_background_image_data("denver,co")
-
+    
     expect(image_data).to be_a(Hash)
     expect(image_data).to have_key(:value)
     expect(image_data[:value]).to be_an(Array)
     expect(image_data[:value]).to be_an(Array)
     expect(image_data[:value].length).to eq(1)
-    expect(image_data[:value][:contentUrl]).to be_a(String)
-    expect(image_data[:value][:hostPageDisplayUrl]).to be_a(String)
-    expect(image_data[:value][:hostPageFavIconUrl]).to be_a(String)
+    expect(image_data[:value][0][:contentUrl]).to be_a(String)
+    expect(image_data[:value][0][:contentUrl]).to eq("https://lovingcities.schottfoundation.org/wp-content/uploads/2018/01/profile-denver.jpg")
+    expect(image_data[:value][0][:hostPageDisplayUrl]).to be_a(String)
+    expect(image_data[:value][0][:hostPageDisplayUrl]).to eq("https://lovingcities.schottfoundation.org/cities/denver-co")
+    expect(image_data[:value][0][:hostPageFavIconUrl]).to be_a(String)
+    expect(image_data[:value][0][:hostPageFavIconUrl]).to eq("https://www.bing.com/th?id=ODF.xluPUAU6PNgXuUquHZGAuA&pid=Api")
   end
 end
