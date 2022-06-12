e090462174380097d4f8598b574a01b56430f9d0
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-12 15:16:55 -0600

    Test: background request responds successfully

---
+++ spec/requests/api/v1/background_mage_request_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Background Image request" do
+  before do
+    get "/api/v1/backgrounds?location=denver,co"
+
+    @image_response = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  it "responds successfully", :vcr do
+    expect(response).ro be_successful
+    expect(@image_response).to be_a(Hash)
+    expect(@image_response[:data][:id]).to eq(nil)
+    expect(@image_response[:data][:type]).to eq("image")
+    expect(@image_response[:data]).to have_key(:attributes)
+    expect(@image_response[:data][:attributes]).to be_a(Hash)
+    expect(@image_response[:data][:attributes]).to have_key(:image)
+  end
+end
