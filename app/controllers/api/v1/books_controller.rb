c863f91b76c07548c36e0afb894bd93b553d1404
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-13 10:25:46 -0600

    Update BooksController index action to render json

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::BooksController < ApplicationController
     @location = LocationFacade.get_location(params[:location])
     @current_weather = WeatherFacade.get_current_weather(@location.lat, @location.lon)
     @books = BookFacade.get_books(params[:location], params[:quantity])
-    binding.pry
+    render json: BooksSerializer.books(@current_weather, @books)
   end
 end
