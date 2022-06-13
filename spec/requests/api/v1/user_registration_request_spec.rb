59b341317b840440dd4fd328c9813304acd620d0
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-13 16:27:54 -0600

    Test(s): User model validations and user create responds successfully

---
+++ spec/requests/api/v1/user_registration_request_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "User registration" do
+  before do
+    headers = {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    }
+    body = {
+      email: "user_1@firstuser.com",
+      password: "user1password",
+      password_confirmation: "user1password"
+    }
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(body)
+  end
+
+  it "responds successfully" do
+    expect(response).to be_successful
+
+    user_data = JSON.parse(response.body, symbolize_names:true)
+
+    expect(user_data[:data][:id]).to be_a(String)
+    expect(user_data[:data][:type]).to eq("user")
+    expect(user_data[:data][:attributes]).to be_a(Hash)
+    expect(user_data[:data][:attributes][:email]).to eq("user_1@firstuser.com")
+    expect(user_data[:data][:attributes]).to have_key(:api_key)
+  end
+end
