b570e74a384c6c0ae54f15798bf32782e10c83a8
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-13 17:59:11 -0600

    Make working password confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,6 @@ class Api::V1::UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
