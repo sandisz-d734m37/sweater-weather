fa69d7dba57d4ad3a0eb5252cb330e894cb8c704
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-13 11:11:36 -0600

    Test: Update book_request_spec to play nice with weather data and VCR

--- spec/requests/api/v1/book_request_spec.rb
+++ spec/requests/api/v1/book_request_spec.rb
@@ -2,12 +2,15 @@ require "rails_helper"
 
 describe "Book request" do
   before do
+    @current_weather = JSON.parse(File.read('spec/fixtures/denver_weather.json'), symbolize_names: true)
+
+    allow(WeatherService).to receive(:get_weather_data).and_return(@current_weather)
     get "/api/v1/book-search?location=denver,co&quantity=3"
 
     @book_response = JSON.parse(response.body, symbolize_names: true)
   end
 
-  it "responds successfully" do
+  it "responds successfully", :vcr do
     expect(response).to be_successful
     expect(@book_response).to be_a(Hash)
     expect(@book_response[:data]).to be_a(Hash)
