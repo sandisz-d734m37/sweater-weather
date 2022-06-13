981ad1f86173b513df8c8f21533d87ba804e940b
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-13 11:59:25 -0600

    Book request returns data for location search 'Nowhere' if no location is entered

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::BooksController < ApplicationController
   def index
+    params[:location] = "Nowhere" unless params[:location]
+    
     @location = LocationFacade.get_location(params[:location])
     @current_weather = WeatherFacade.get_current_weather(@location.lat, @location.lon)
     @books = BookFacade.get_books(params[:location], params[:quantity])
