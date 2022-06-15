c1a76ec0189466749f410863fb6c9bbbd1892c80
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-14 19:17:46 -0600

    Impossible road trip fails gracefully

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,12 +3,17 @@ class Api::V1::RoadTripController < ApplicationController
     user = User.find_by(api_key: params[:api_key])
     if user
       route = RouteFacade.get_route(params[:origin], params[:destination])
-      
-      weather = find_weather(route, route.end_lat, route.end_lon)
 
-      road_trip = user.road_trips.create(start_city: route.start_city, end_city: route.end_city, travel_time: route.travel_time)
-      if road_trip.save
-        render json: RoadTripSerializer.create_road_trip(road_trip, weather), status: :created
+      if route.class == String
+        render json: RoadTripSerializer.road_trip_impossible(params[:origin], params[:destination])
+      else
+
+        weather = find_weather(route, route.end_lat, route.end_lon)
+
+        road_trip = user.road_trips.create(start_city: route.start_city, end_city: route.end_city, travel_time: route.travel_time)
+        if road_trip.save
+          render json: RoadTripSerializer.create_road_trip(road_trip, weather), status: :created
+        end
       end
     end
   end
