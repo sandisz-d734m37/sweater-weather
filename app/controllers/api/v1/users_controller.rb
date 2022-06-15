ac4d0a6f98881af31f576f61ba8ac05dca0f7937
Sandisz Thieme <sandiszthieme@gmail.com>
2022-06-14 21:48:05 -0600

    User registration returns 409 error code is user already exists

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,8 @@ class Api::V1::UsersController < ApplicationController
 
     if user.save
       render json: UserSerializer.register(user), status: :created
+    elsif User.find_by(email: params[:email])
+      render json: ErrorSerializer.error_with_status(:already_exists), status: 409
     end
   end
 
--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -5,7 +5,8 @@ class ErrorSerializer
     def error_with_status(error_status)
       error_hash = {
         api_key: invalid_api_key,
-        credentials: invalid_credentials
+        credentials: invalid_credentials,
+        already_exists: user_already_exists
       }
 
       error_hash[error_status]
@@ -27,6 +28,16 @@ class ErrorSerializer
           error_code: 401,
           type: "error",
           error_mesage: "Invalid log in credentials"
+        }
+      }
+    end
+
+    def user_already_exists
+      {
+        data: {
+          error_code: 409,
+          type: "error",
+          error_mesage: "Conflict: user already exists"
         }
       }
     end
